600ad4d47fa6e038b871b4cd8b7548ef07a91dfe
vaguelyr <greymoth90@gmail.com>
2019-03-18 18:26:54 -0500

    Now supports comments.

--- well.sh
+++ well.sh
@@ -8,10 +8,13 @@
 
 maxdl=3
 while read line ; do
+	echo $line | grep '#' && continue # skip comments
 	echo line $line
 	youtube-dl $index  --extract-audio --audio-format aac --audio-quality 5 --max-downloads $maxdl
 done <<- EOF
+	# this is a comment
 	url1
+	# comments are ignored
 	url2
 	ulr3
 EOF
